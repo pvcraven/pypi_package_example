26f1fdf2132108c08907e513afddd59bd5e02c77
Paul Vincent Craven <paul@cravenfamily.com>
2019-11-02 15:57:15 -0500

    Update

---
+++ tests/test_my_function.py
@@ -0,0 +1,10 @@
+import pytest
+import sys
+import pypi_package_example
+
+
+def test_my_function(capfd):
+    pypi_package_example.my_function()
+
+    out, err = capfd.readouterr()
+    assert out == "Hi\n"
